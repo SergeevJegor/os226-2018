08a006997302c1bc1ac2173242317d0aca52dd06
Sergeev Jegor <egor.f.sergeev@gmail.com>
2018-09-09 14:30:53 +0300

    Implemented string functional

--- libc/string.c
+++ libc/string.c
@@ -2,32 +2,91 @@
 #include <stddef.h>
 #include <string.h>
 
-static inline __attribute__((noreturn)) void IMPL_ME() {
-	extern void hal_halt(void);
-	hal_halt();
-	while(1);
-}
-
 int strcmp(const char *s1, const char *s2) {
-	IMPL_ME();
+    while (*s1 && *s2 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+
+    return *s1 - *s2;
 }
 
 size_t strlen(const char *s) {
-	IMPL_ME();
+    size_t len = 0;
+    while (*s) {
+        s++;
+        len++;
+    }
+
+    return len;
 }
 
 char *strchr(const char *s, int c) {
-	IMPL_ME();
+    while (*s && (*s != c)) {
+        s++;
+    }
+    if (!*s)
+        return NULL;
+    else
+        return (char *) s;
 }
 
 size_t strspn(const char *str, const char *accept) {
-	IMPL_ME();
+    size_t length = 0;
+    while (*str) {
+        char *a = (char *) accept;
+        while (*a && *str != *a) {
+            a++;
+        }
+        if (*a) {
+            str++;
+            length++;
+        }
+        else {
+            return length;
+        }
+    }
+    return length;
 }
 
 size_t strcspn(const char *str, const char *reject) {
-	IMPL_ME();
+    size_t length = 0;
+    while (*str) {
+        char *r = (char *) reject;
+        while (*r && *str != *r) {
+            r++;
+        }
+        if (*r) {
+            return length;
+        }
+        else {
+            str++;
+            length++;
+        }
+    }
+    return length;
 }
 
 char *strtok_r(char *str, const char *delim, char **saveptr) {
-	IMPL_ME();
+    if (!str) {
+        if (!*saveptr)
+            return NULL;
+        str = *saveptr;
+    }
+    while (*str && strcspn(str, delim) == 0) {
+        str++;
+    }
+    if (!*str) {
+        return NULL;
+    }
+    char *delimiter_symbol;
+    delimiter_symbol = str + strcspn(str, delim);
+    if (*delimiter_symbol) {
+        *delimiter_symbol = '\0';
+        *saveptr = delimiter_symbol + 1;
+    }
+    else {
+        *saveptr = delimiter_symbol;
+    }
+    return str;
 }
